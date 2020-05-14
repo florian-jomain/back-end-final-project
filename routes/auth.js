be0eea29f3a6f18581c805746914d5053d2a546a
Florian Jomain <florian.jomain@gmail.com>
2020-05-14 12:07:27 +0200

    feat: BE - Auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,25 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const upload = require("../config/cloudinaryConfig");
 
 const salt = 10;
 
 router.post("/signin", (req, res, next) => {
   const { email, password } = req.body;
-  User.findOne({ email }).then((userDocument) => {
+  User.findOne({
+    email,
+  }).then((userDocument) => {
     if (!userDocument) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
     }
     const isValidPassword = bcrypt.compareSync(password, userDocument.password);
     if (!isValidPassword) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
     }
     const userObj = userDocument.toObject();
     delete userObj.password;
@@ -22,22 +29,60 @@ router.post("/signin", (req, res, next) => {
   });
 });
 
-router.post("/signup", (req, res, next) => {
-  const { email, password, firstName, lastName } = req.body;
+router.post("/signup/helper", upload.single("image"), (req, res, next) => {
+  const { email, password, name } = req.body;
+  const newHelper = { email, password, name };
 
-  User.findOne({ email }).then((userDocument) => {
+  if (req.file) {
+    newHelper.image = req.file.secure_url;
+  }
+
+  Helper.findOne({
+    email,
+  }).then((userDocument) => {
     if (userDocument) {
-      return res.status(400).json({ message: "Email already taken" });
+      return res.status(400).json({
+        message: "Email already taken",
+      });
     }
 
     const hashedPassword = bcrypt.hashSync(password, salt);
-    const newUser = { email, lastName, firstName, password: hashedPassword };
+    newHelper.password = hashedPassword;
 
-    User.create(newUser).then((newUserDocument) => {
-      const userObj = newUserDocument.toObject();
-      delete userObj.password;
-      req.session.currentUser = userObj;
-      res.status(201).json(userObj);
+    Helper.create(newHelper).then((newHelperDocument) => {
+      const helperObj = newHelperDocument.toObject();
+      delete helperObj.password;
+      req.session.currentUser = helperObj;
+      res.status(201).json(helperObj);
+    });
+  });
+});
+
+router.post("/signup/charity", (req, res, next) => {
+  const { email, password, name } = req.body;
+  const newCharity = { email, password, name };
+
+  if (req.file) {
+    newCharity.image = req.file.secure_url;
+  }
+
+  Charity.findOne({
+    email,
+  }).then((userDocument) => {
+    if (userDocument) {
+      return res.status(400).json({
+        message: "Email already taken",
+      });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    newCharity.password = hashedPassword;
+
+    Charity.create(newCharity).then((newCharityDocument) => {
+      const charityObj = newCharityDocument.toObject();
+      delete charityObj.password;
+      req.session.currentUser = charityObj;
+      res.status(201).json(charityObj);
     });
   });
 });
@@ -45,24 +90,37 @@ router.post("/signup", (req, res, next) => {
 router.get("/isLoggedIn", (req, res, next) => {
   if (req.session.currentUser) {
     const id = req.session.currentUser._id;
-    User.findById(id)
-      .then((userDocument) => {
-        const userObj = userDocument.toObject();
-        delete userObj.password;
-        res.status(200).json(userObj);
+    Helper.findById(id)
+      .then((helperDocument) => {
+        const helperObj = helperDocument.toObject();
+        delete helperObj.password;
+        res.status(200).json(helperObj);
       })
-      .catch((error) => {
-        res.status(401).json(error);
+      .catch(() => {
+        Charity.findById(id)
+          .then((charityDocument) => {
+            const charityObj = charityDocument.toObject();
+            delete charityObj.password;
+            res.status(200).json(charityObj);
+          })
+          .catch((error) => {
+            res.status(401).json(error);
+          });
       });
   } else {
-    res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({
+      message: "Unauthorized",
+    });
   }
 });
 
 router.get("/logout", (req, res, next) => {
   req.session.destroy(function (error) {
     if (error) res.status(500).json(error);
-    else res.status(200).json({ message: "Succesfully disconnected." });
+    else
+      res.status(200).json({
+        message: "Succesfully disconnected.",
+      });
   });
 });
 
