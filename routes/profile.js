2386aa0692b7c5be8efb5efca6b3a29a759e72cc
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-20 18:09:54 +0200

    feat: updated route to get one charity and helper

--- routes/profile.js
+++ routes/profile.js
@@ -3,142 +3,165 @@
 // GET - /api/helpers - Get all helpers
 // GET - /api/charities - Get all charities
 
-const express = require("express");
-const router = express.Router();
-const upload = require("../config/cloudinaryConfig");
-const Helper = require("../models/Helper");
-const Charity = require("../models/Charity");
-const requireAuth = require("../middlewares/requireAuth");
+const express = require('express')
+const router = express.Router()
+const upload = require('../config/cloudinaryConfig')
+const Helper = require('../models/Helper')
+const Charity = require('../models/Charity')
+const requireAuth = require('../middlewares/requireAuth')
 
-router.patch('/helpers/create/', requireAuth, upload.single("image"), (req, res, next) => {
+// GET ONE HELPER
+router.get('/user/:id', async (req, res, next) => {
+  const id = req.params.id
+  try {
+    let helper = await findHelper(id)
+    if (!helper) {
+      helper = await findCharity(id)
+    }
+    res.status(200).json(helper)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+})
 
+function findHelper(id) {
+  return Helper.findById(id)
+}
+
+function findCharity(id) {
+  return Charity.findById(id)
+}
+
+router.patch(
+  '/helpers/create/',
+  requireAuth,
+  upload.single('image'),
+  (req, res, next) => {
     if (req.file) {
-        req.body.image = req.file.secure_url;
+      req.body.image = req.file.secure_url
     }
     if (req.body.links) {
-        req.body.links = req.body.links.split(",")
+      req.body.links = req.body.links.split(',')
     }
 
     if (req.body.skills) {
-        req.body.skills = req.body.skills.split(",")
+      req.body.skills = req.body.skills.split(',')
     }
 
-    Helper.findByIdAndUpdate(
+    Helper.findByIdAndUpdate(req.session.currentUser._id, req.body, {
+      new: true,
+    })
+      // .populate('id_projects')
+      // .populate('skills', 'label')
+      .then((apiResponse) => {
+        res.status(200).json(apiResponse)
+      })
+      .catch((apiError) => {
+        console.log(apiError)
+        res.status(500).json(apiError)
+      })
+  }
+)
 
-            req.session.currentUser._id,
-            req.body, {
-                new: true
-            }
-        )
-        // .populate('id_projects')
-        // .populate('skills', 'label')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            console.log(apiError)
-            res.status(500).json(apiError);
-        })
-});
-
-router.patch('/helpers/:id', requireAuth, upload.single("image"), (req, res, next) => {
-
+router.patch(
+  '/helpers/:id',
+  requireAuth,
+  upload.single('image'),
+  (req, res, next) => {
     if (req.file) {
-        req.body.image = req.file.secure_url;
+      req.body.image = req.file.secure_url
     }
     if (req.body.links) {
-        req.body.links = req.body.links.split(",")
+      req.body.links = req.body.links.split(',')
     }
 
     if (req.body.skills) {
-        req.body.skills = req.body.skills.split(",")
+      req.body.skills = req.body.skills.split(',')
     }
 
-    Helper.findByIdAndUpdate(
-            req.params.id,
-            req.body, {
-                new: true
-            }
-        )
-        .populate('id_projects')
-        .populate('skills', 'label')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            res.status(500).json(apiError);
-        })
-});
+    Helper.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .populate('id_projects')
+      .populate('skills', 'label')
+      .then((apiResponse) => {
+        res.status(200).json(apiResponse)
+      })
+      .catch((apiError) => {
+        res.status(500).json(apiError)
+      })
+  }
+)
 
-router.patch('/charities/create/', requireAuth, upload.single("image"), (req, res, next) => {
+router.patch(
+  '/charities/create/',
+  requireAuth,
+  upload.single('image'),
+  (req, res, next) => {
     if (req.file) {
-        req.body.image = req.file.secure_url;
+      req.body.image = req.file.secure_url
     }
     if (req.body.links) {
-        req.body.links = req.body.links.split(",")
+      req.body.links = req.body.links.split(',')
     }
 
-    Charity.findByIdAndUpdate(
-            req.session.currentUser._id,
-            req.body, {
-                new: true
-            }
-        )
-        // .populate('id_projects')
-        // .populate('skills', 'label')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            res.status(500).json(apiError);
-        })
-});
+    Charity.findByIdAndUpdate(req.session.currentUser._id, req.body, {
+      new: true,
+    })
+      // .populate('id_projects')
+      // .populate('skills', 'label')
+      .then((apiResponse) => {
+        res.status(200).json(apiResponse)
+      })
+      .catch((apiError) => {
+        res.status(500).json(apiError)
+      })
+  }
+)
 
-
-
-router.patch('/charities/:id', requireAuth, upload.single("image"), (req, res, next) => {
-
+router.patch(
+  '/charities/:id',
+  requireAuth,
+  upload.single('image'),
+  (req, res, next) => {
     if (req.file) {
-        req.body.image = req.file.secure_url;
+      req.body.image = req.file.secure_url
     }
 
-    Charity.findByIdAndUpdate(
-            req.params.id,
-            req.body, {
-                new: true
-            }
-        )
-        .populate('id_projects')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            res.status(500).json(apiError);
-        })
-});
-
-
+    Charity.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .populate('id_projects')
+      .then((apiResponse) => {
+        res.status(200).json(apiResponse)
+      })
+      .catch((apiError) => {
+        res.status(500).json(apiError)
+      })
+  }
+)
 
 router.get('/helpers', (req, res, next) => {
-    Helper.find()
-        .populate('id_projects')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            res.status(500).json(apiError);
-        })
+  Helper.find()
+    .populate('id_projects')
+    .then((apiResponse) => {
+      res.status(200).json(apiResponse)
+    })
+    .catch((apiError) => {
+      res.status(500).json(apiError)
+    })
 })
 
 router.get('/charities', (req, res, next) => {
-    Charity.find()
-        .populate('id_projects')
-        .then(apiResponse => {
-            res.status(200).json(apiResponse);
-        })
-        .catch(apiError => {
-            res.status(500).json(apiError);
-        })
+  Charity.find()
+    .populate('id_projects')
+    .then((apiResponse) => {
+      res.status(200).json(apiResponse)
+    })
+    .catch((apiError) => {
+      res.status(500).json(apiError)
+    })
 })
 
-module.exports = router;
+module.exports = router
