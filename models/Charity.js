2f969c317e4044f67836d733099b0a767bbabb1e
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-20 11:20:20 +0200

    feat: add user type on models

--- models/Charity.js
+++ models/Charity.js
@@ -1,35 +1,38 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const charitySchema = new Schema({
-    email: {
-        type: String,
-        required: true
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: '../../media/avatar-charity.png',
+  },
+  bio: String,
+  links: [String],
+  id_projects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
     },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-    },
-    image: {
-        type: String,
-        default: ""
-    },
-    bio: String,
-    links: [String],
-    id_projects: [{
-        type: Schema.Types.ObjectId,
-        ref: "Project"
-    }],
-    location: String,
-});
+  ],
+  location: String,
+  userType: String,
+})
 
-const Charity = mongoose.model("Charity", charitySchema);
+const Charity = mongoose.model('Charity', charitySchema)
 
-module.exports = Charity;
+module.exports = Charity
--- models/Helper.js
+++ models/Helper.js
@@ -1,39 +1,42 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const helperSchema = new Schema({
-    email: {
-        type: String,
-        required: true
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: '../../media/avatar.png',
+  },
+  skills: [String],
+  bio: String,
+  location: String,
+  phone: String,
+  links: [String],
+  id_projects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
     },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-    },
-    image: {
-        type: String,
-        default: '../../media/avatar.png',
-    },
-    skills: [String],
-    bio: String,
-    location: String,
-    phone: String,
-    links: [String],
-    id_projects: [{
-        type: Schema.Types.ObjectId,
-        ref: "Project"
-    }],
-    feedback: String,
-    stars: Number,
-});
+  ],
+  feedback: String,
+  stars: Number,
+  userType: String,
+})
 
-const Helper = mongoose.model("Helper", helperSchema);
+const Helper = mongoose.model('Helper', helperSchema)
 
-module.exports = Helper;
+module.exports = Helper
